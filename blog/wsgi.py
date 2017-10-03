436147577d3dd7607bdeeaa1af720d2602dde2cc
mudabir <mudabir.shakil@gmail.com>
2017-10-03 17:54:56 +0500

    updated python version

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -1,4 +1,19 @@
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+"""
+WSGI config for blog project.
 
-application = Cling(get_wsgi_application())
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
+
+application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
