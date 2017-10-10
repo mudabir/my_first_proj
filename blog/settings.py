bb8710f57ebbb29e9d1e4ef9d2996c7e4f7fd2a3
mudabir <mudabir.shakil@gmail.com>
2017-10-10 12:36:34 +0500

    add statics

--- blog/settings.py
+++ blog/settings.py
@@ -134,8 +134,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
 #this is required for admin    'django.core.context_processors.csrf', #necessary for csrf protection    'django.core.context_processors.static',    'django.contrib.messages.context_processors.messages',    'django.core.context_processors.debug',)
 
 STATICFILES_DIRS = (
@@ -149,6 +147,6 @@ STATICFILES_FINDERS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = "/"#os.path.join(REPOSITORY_ROOT, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
